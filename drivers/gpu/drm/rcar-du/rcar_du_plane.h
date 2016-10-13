5345cb181a40d0d916460ce6b28a2363a0a959d0
Koji Matsuoka <koji.matsuoka.xm@renesas.com>
2016-10-13 13:55:29 +0900

    drm: rcar-du: Fix DPLL for HDMI dotclock support
    Signed-off-by: Koji Matsuoka <koji.matsuoka.xm@renesas.com>

--- drivers/gpu/drm/rcar-du/rcar_du_plane.h
+++ drivers/gpu/drm/rcar-du/rcar_du_plane.h
@@ -34,6 +34,11 @@ enum rcar_du_plane_source {
 	RCAR_DU_PLANE_VSPD1,
 };
 
+#define DU_CH_0		0
+#define DU_CH_1		1
+#define DU_CH_2		2
+#define DU_CH_3		3
+
 struct rcar_du_plane {
 	struct drm_plane plane;
 	struct rcar_du_group *group;
