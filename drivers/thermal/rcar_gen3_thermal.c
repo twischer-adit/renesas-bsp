renesas-rcar/linux-bsp

thermal: rcar_gen3_thermal: Add support for r8a77965
Hardware structure and operation of r8a77965 are the same with r8a7796.
So, compatible structure of r8a7796 is reused.

Signed-off-by: Dien Pham <dien.pham.ry@renesas.com>

drivers/thermal/rcar_gen3_thermal.c
@@ -475,6 +475,7 @@ static const struct rcar_thermal_data r8a7796_data = {
 static const struct of_device_id rcar_thermal_dt_ids[] = {
 	{ .compatible = "renesas,thermal-r8a7795", .data = &r8a7795_data},
 	{ .compatible = "renesas,thermal-r8a7796", .data = &r8a7796_data},
+	{ .compatible = "renesas,thermal-r8a77965", .data = &r8a7796_data},
 	{},
 };
 MODULE_DEVICE_TABLE(of, rcar_thermal_dt_ids);

twischer-adit/renesas-bsp

Signed-off-by: An Huynh <an.huynh.uj@rvc.renesas.com>

drivers/thermal/rcar_gen3_thermal.c
@@ -371,6 +371,7 @@ static const struct rcar_gen3_thermal_data r8a7796_data = {
 static const struct of_device_id rcar_gen3_thermal_dt_ids[] = {
 	{ .compatible = "renesas,r8a7795-thermal", .data = &r8a7795_data},
 	{ .compatible = "renesas,r8a7796-thermal", .data = &r8a7796_data},
+	{ .compatible = "renesas,r8a77965-thermal", .data = &r8a7796_data},
 	{},
 };
 MODULE_DEVICE_TABLE(of, rcar_gen3_thermal_dt_ids);