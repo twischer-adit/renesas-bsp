renesas-rcar/linux-bsp

spi: sh-msiof: Add support for r8a77965 (R-Car M3-N)
This patch adds a compatible string for R-Car M3-N support.

Signed-off-by: Hiromitsu Yamasaki <hiromitsu.yamasaki.ym@renesas.com>

drivers/spi/spi-sh-msiof.c
@@ -205,7 +205,7 @@ static const struct soc_device_attribute rcar_quirks_match[]  = {
 		.data = (void *)TRANSFER_WORKAROUND_H3WS10, },
 	{ .soc_id = "r8a7795", .revision = "ES1.1",
 		.data = (void *)TRANSFER_WORKAROUND_H3WS11, },
-	{ .soc_id = "r8a7796",
+	{ .soc_id = "r8a7796*",
 		.data = (void *)UNDIVIDED_PROHIBIT, },
 	{/*sentinel*/},
 };
@@ -215,7 +215,8 @@ static int msiof_rcar_is_gen3(struct device *dev)
 	struct device_node *node = dev->of_node;
 
 	return of_device_is_compatible(node, "renesas,msiof-r8a7795") ||
-		of_device_is_compatible(node, "renesas,msiof-r8a7796");
+		of_device_is_compatible(node, "renesas,msiof-r8a7796") ||
+		of_device_is_compatible(node, "renesas,msiof-r8a77965");
 }
 
 static u32 sh_msiof_read(struct sh_msiof_spi_priv *p, int reg_offs)
@@ -1188,6 +1189,7 @@ static const struct of_device_id sh_msiof_match[] = {
 	{ .compatible = "renesas,msiof-r8a7794",   .data = &r8a779x_data },
 	{ .compatible = "renesas,msiof-r8a7795",   .data = &r8a779x_data },
 	{ .compatible = "renesas,msiof-r8a7796",   .data = &r8a779x_data },
+	{ .compatible = "renesas,msiof-r8a77965",  .data = &r8a779x_data },
 	{},
 };
 MODULE_DEVICE_TABLE(of, sh_msiof_match);

twischer-adit/renesas-bsp

drivers/spi/spi-sh-msiof.c
@@ -213,7 +213,8 @@ static int msiof_rcar_is_gen3(struct device *dev)
 	struct device_node *node = dev->of_node;
 
 	return of_device_is_compatible(node, "renesas,msiof-r8a7795") ||
-		of_device_is_compatible(node, "renesas,msiof-r8a7796");
+		of_device_is_compatible(node, "renesas,msiof-r8a7796") ||
+		of_device_is_compatible(node, "renesas,msiof-r8a77965");
 }
 
 static u32 sh_msiof_read(struct sh_msiof_spi_priv *p, int reg_offs)
@@ -1167,6 +1168,7 @@ static const struct of_device_id sh_msiof_match[] = {
 	{ .compatible = "renesas,rcar-gen2-msiof", .data = &rcar_gen2_data },
 	{ .compatible = "renesas,msiof-r8a7795",   .data = &rcar_gen3_data },
 	{ .compatible = "renesas,msiof-r8a7796",   .data = &rcar_gen3_data },
+	{ .compatible = "renesas,msiof-r8a77965",  .data = &rcar_gen3_data },
 	{ .compatible = "renesas,rcar-gen3-msiof", .data = &rcar_gen3_data },
 	{ .compatible = "renesas,sh-msiof",        .data = &sh_data }, /* Deprecated */
 	{},